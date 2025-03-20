11172f875e4f41cddf650b877c18c801605cdd7b
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2025-03-20 11:52:47 +0100

    Random review fix

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -7,17 +7,16 @@ function App (){
   const [allReviews, setReviews] = useState(reviews)
   const [activeReview, setActivereview] = useState([])
 
-
   const getActiveReview = (id)=>{
     console.log(id)
+    if(id === activeReview.id){
+      id += 1
+    }
     if(id <= 0){
       id = allReviews.length
     }
     if( id > allReviews.length){
       id = 1
-    }
-    if(id === activeReview.id){
-      id += 1
     }
 
     const currentReview = allReviews.find((review)=>review.id === id)
--- reviews/src/components/Reviews.jsx
+++ reviews/src/components/Reviews.jsx
@@ -2,24 +2,24 @@ import arrowBtn from '../assets/images/arrowBtn.svg'
 
 export const Reviews = ({activeReview, getActiveReview, allReviews}) => {
     const randomId = Math.floor(Math.random() * allReviews.length - 1) + 1;
-    
+
     return (
         <article className='py-[24px] px-[36px] bg-[#FFFFFF] shadow-2xl w-[90%] max-w-[600px]'>
-            <div className='w-[150px] h-[150px] rounded-[100%] mx-auto relative bg-[white] overflow-hidden'>
-            <img src={activeReview.image} alt="" className='w-full h-full object-cover'/>
+            <div className="relative w-[150px] h-[150px] rounded-full overflow-hidden before:content-[''] before:absolute before:w-full before:h-full before:bg-[#645CFF] before:rounded-full before:-top-1 before:-right-2 mx-auto before:z-[-1]">
+                <img src={activeReview.image} alt={activeReview.name} className='w-full h-full object-cover !z-10'/>
             </div>
             <div className='text-center mt-[1rem]'>
-            <h3 className='text-[#0F172A] tracking-[1px] text-[25px] leading-[25.01px] capitalize font-bold'>{activeReview.name}</h3>
-            <p className="text-[#645CFF] tracking-[1px] text-[13px] leading-[13.01px] mt-[0.5rem] mb-[13px] uppercase font-[500]">{activeReview.job}</p>
-            <p className="text-[#475569] text-[16px] leading-[24px] font-[500]">
-            {activeReview.text}
-            </p>
+                <h3 className='text-[#0F172A] tracking-[1px] text-[25px] leading-[25.01px] capitalize font-bold'>{activeReview.name}</h3>
+                <p className="text-[#645CFF] tracking-[1px] text-[13px] leading-[13.01px] mt-[0.5rem] mb-[13px] uppercase font-[500]">{activeReview.job}</p>
+                <p className="text-[#475569] text-[16px] leading-[24px] font-[500]">
+                {activeReview.text}
+                </p>
             </div>
             <div className="navBtns flex items-center justify-center mt-[1rem] gap-[20px] mb-[24px]">
-            <img src={arrowBtn} alt="" onClick={()=>getActiveReview(activeReview.id - 1)}/>
-            <img src={arrowBtn} alt="" className='rotate-[180deg]' onClick={()=>getActiveReview(activeReview.id + 1)}/>
+                <img src={arrowBtn} alt="" onClick={()=>getActiveReview(activeReview.id - 1)}/>
+                <img src={arrowBtn} alt="" className='rotate-[180deg]' onClick={()=>getActiveReview(activeReview.id + 1)}/>
             </div>
-            <button className='bg-[#C1BEFF] rounded-[4px] text-[#645CFF] text-[13px] tracking-[1px] py-[6px] px-[12px] mx-auto w-fit block capitalize' onClick={()=>getActiveReview(randomId)}>suprise me</button>
+            <button className='bg-[#C1BEFF] rounded-[4px] text-[#645CFF] text-[13px] tracking-[1px] py-[6px] px-[12px] mx-auto w-fit block capitalize cursor-pointer' onClick={()=>getActiveReview(randomId)}>suprise me</button>
         </article>
     )
 }
