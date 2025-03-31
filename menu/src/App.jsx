ebc520372c3a43d4405cad689ea44f2ec05908ea
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2025-03-31 12:14:27 +0100

    Fix item stretch

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -35,21 +35,21 @@ function App (){
           })
         }
       </div>
-      <div className="menu-items flex items-center justify-center flex-wrap gap-[2rem]">
+      <div className="menu-items flex justify-center flex-wrap gap-[2rem] items-stretch">
         {
           menuItems.map((menu)=>{
             return (
-              <article key={menu.id} className="menu-item w-full max-w-[367px] bg-[#FFFFFF] rounded-[4px] shadow-2xl overflow-hidden">
+              <article key={menu.id} className="menu-item w-full max-w-[367px] bg-[#FFFFFF] rounded-[4px] shadow-2xl overflow-hidden flex flex-col flex-grow">
                 <div className="h-[240px] overflow-hidden">
                   <img src={menu.img} alt={menu.title} className="w-full h-full object-cover"/>
                 </div>
                 <div className="p-[1.5rem]">
                   <div className="flex items-center justify-between mb-[1.2rem]">
-                    <h3 className="font-[500] text-[1.2rem] text-[#0F172A] capitalize leading-[1.25rem]">buttermilk pancakes</h3>
-                    <span className="bg-[#F59E0B] text-[#FFFFFF] px-[8px] py-[4px] text-[1rem] rounded-[4px]">$15.99</span>
+                    <h3 className="font-[500] text-[1.2rem] text-[#0F172A] capitalize leading-[1.25rem]">{menu.title}</h3>
+                    <span className="bg-[#F59E0B] text-[#FFFFFF] px-[8px] py-[4px] text-[1rem] rounded-[4px]">${menu.price}</span>
                   </div>
                   <p className="text-[#64748B] leading-[2rem] text-[1rem]">
-                    I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed
+                   {menu.desc}
                   </p>
                 </div>
               </article>
